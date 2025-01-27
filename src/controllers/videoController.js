d3cc7af0293855203cb5e8fb49338fde56f8dced
sg_yun <139228985+sg-yun71@users.noreply.github.com>
2025-01-27 22:54:40 +0900

    update

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,19 @@
-export const trending = (req,res) => res.send("Home Page Videos");
-export const watch = (req,res) => res.send("Watch");
-export const edit = (req,res) => res.send("Edit");
-
+export const trending = (req, res) =>
+    res.send(
+      "<!DOCTYPE html><html lang='ko'><head><title>Youtube</title></head><body><h1>Home</h1><footer>&copy;2024 Youtube -  All rights reserved</footer></body></html>"
+    );
+export const see = (req, res) => {
+    return res.send(
+        `<!DOCTYPE html><html lang='ko'><head><title>Youtube</title></head><body><h1>Watch video #${req.params.id}</h1><footer>&copy;2024 Youtube -  All rights reserved</footer></body></html>`
+    );
+};
+export const edit = (req, res) => {
+    return res.send(
+        `<!DOCTYPE html><html lang='ko'><head><title>Youtube</title></head><body><h1>Edit video #${req.params.id}</h1><footer>&copy;2024 Youtube -  All rights reserved</footer></body></html>`
+    );
+};
+export const search = (req, res) => res.send("Search");
+export const upload = (req, res) => res.send("Upload");
+export const deleteVideo = (req, res) => {
+  return res.send("Delete Video");
+};
--- src/routers/globelRouter.js
+++ src/routers/globelRouter.js
@@ -1,10 +1,12 @@
 import express from "express";
-import {join} from "../controllers/userController"
-import {trending} from "../controllers/videoController"
+import {join, login} from "../controllers/userController"
+import {trending, search} from "../controllers/videoController"
 
 const globelRouter = express.Router();
 
 globelRouter.get("/", trending);
 globelRouter.get("/join", join);
+globelRouter.get("/login", login);
+globelRouter.get("/search", search);
 
 export default globelRouter;
