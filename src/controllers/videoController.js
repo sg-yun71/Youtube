88bf91731f06223d59a00adc6ed1a146a6267fc3
sg_yun <139228985+sg-yun71@users.noreply.github.com>
2025-01-30 04:03:57 +0900

    update

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,17 +1,34 @@
-export const trending = (req, res) =>
-    res.send(
-      "<!DOCTYPE html><html lang='ko'><head><title>Youtube</title></head><body><h1>Home</h1><footer>&copy;2024 Youtube -  All rights reserved</footer></body></html>"
-    );
-export const see = (req, res) => {
-    return res.send(
-        `<!DOCTYPE html><html lang='ko'><head><title>Youtube</title></head><body><h1>Watch video #${req.params.id}</h1><footer>&copy;2024 Youtube -  All rights reserved</footer></body></html>`
-    );
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "First Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Second Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Third Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+  ];
+  return res.render("home", { pageTitle: "Home", videos });
 };
-export const edit = (req, res) => {
-    return res.send(
-        `<!DOCTYPE html><html lang='ko'><head><title>Youtube</title></head><body><h1>Edit video #${req.params.id}</h1><footer>&copy;2024 Youtube -  All rights reserved</footer></body></html>`
-    );
-};
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => {
