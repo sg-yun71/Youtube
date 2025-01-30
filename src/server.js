70facdaefa4f0a6a707f346c399944362049bdf8
sg_yun <139228985+sg-yun71@users.noreply.github.com>
2025-01-30 15:52:44 +0900

    update

--- src/server.js
+++ src/server.js
@@ -1,24 +1,37 @@
 import express from "express";
 import morgan from "morgan";
-import globelRouter from "./routers/globelRouter";
-import userRouter from "./routers/userRouter";
+import session from "express-session";
+import flash from "express-flash";
+import MongoStore from "connect-mongo";
+import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
+import apiRouter from "./routers/apiRouter";
+import { localsMiddleware } from "./middlewares";
 
-const PORT = 4000;
-
-console.log(process.cwd());
-
-const app  = express();
+const app = express();
 const logger = morgan("dev");
 
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
-app.use("/", globelRouter);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
+  })
+);
+app.use(flash());
+app.use(localsMiddleware);
+app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("assets"));
+app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
+app.use("/api", apiRouter);
 
-const handleListening = () => 
-    console.log('Server listening on port http://localhost: ${PORT} ');
-
-app.listen(4000, handleListening);
+export default app;
